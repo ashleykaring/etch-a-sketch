b0a65ec3efe7617d5fec428ab2f01b323238501f
ashleykaring <aaring@calpoly.edu>
2024-08-30 22:21:32 +0000

    basic game done

---
+++ jsfile.js
@@ -0,0 +1,36 @@
+const DEFAULT_SIZE = 16;
+
+window.onload = () => makeGrid(DEFAULT_SIZE);
+
+function makeGrid(size) {
+    let container = document.querySelector(".container");
+    container.innerHTML = "";
+
+    let elementSize = 500 / size;
+
+    for (let i = 1; i <= size ** 2; i++) {
+        let gridElement = document.createElement("div");
+        gridElement.classList.add("gridElement");
+        gridElement.style.width = `${elementSize}px`;
+        gridElement.style.height = `${elementSize}px`;
+        gridElement.style.border = "1px solid black";
+
+        gridElement.addEventListener("click", () => gridElement.style.backgroundColor = "yellow")
+
+        container.appendChild(gridElement);
+    }
+}
+
+let button = document.getElementById("new");
+button.addEventListener("click", () => newGrid(prompt("Please enter the width/height")));
+
+function newGrid(size) {
+    let container = document.querySelector(".container");
+    container.innerHTML = "";
+    size = parseInt(size);
+    if (size > 100 || size < 1 || isNaN(size)) {
+        alert("Size not valid, needs to be a number between 1 and 100");
+    } else {
+        makeGrid(size);
+    }
+}
